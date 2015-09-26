dac6a4b162acad58b32fb56cf2d5440f5bcce447
Stephan Hoyer <shoyer@climate.com>
2015-09-26 13:54:20 -0600

    KNN

--- regressor.py
+++ regressor.py
@@ -1,12 +1,13 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator
+from sklearn.neighbors import KNeighborsRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = LinearRegression()
+        self.clf = KNeighborsRegressor()
 
     def fit(self, X, y):
-        self.clf.fit(X, y)
+        self.clf.fit(X[:, 1:], X[:, 0])
 
     def predict(self, X):
-        return self.clf.predict(X)
+        return self.clf.predict(X[:, 1:])
--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -13,26 +13,24 @@ def get_enso_mean(tas):
     """The array of mean temperatures in the El Nino 3.4 region at all time points."""
     return get_area_mean(tas, en_lat_bottom, en_lat_top, en_lon_left, en_lon_right)
 
-class FeatureExtractor(object):
 
+class FeatureExtractor(object):
     def __init__(self):
         pass
 
+    def fit(self, temperatures_xray, n_burn_in, n_lookahead):
+        pass
+
     def transform(self, temperatures_xray, n_burn_in, n_lookahead, skf_is):
-        """Compute the single variable of mean temperatures in the El Nino 3.4
-        region."""
-        # This is the range for which features should be provided. Strip
-        # the burn-in from the beginning and the prediction look-ahead from
-        # the end.
+        """Use world temps as features."""
+        # Set all temps on world map as features
+        all_temps = temperatures_xray['tas'].values
+        time_steps, lats, lons = all_temps.shape
+        all_temps = all_temps.reshape((time_steps, lats * lons))
+        all_temps = all_temps[n_burn_in:-n_lookahead, :]
+
         valid_range = range(n_burn_in, temperatures_xray['time'].shape[0] - n_lookahead)
         enso = get_enso_mean(temperatures_xray['tas'])
         enso_valid = enso.values[valid_range]
 
-
-        year_fraction = (np.array(valid_range) % 12) / 12.0
-        seasonal_features = np.vstack(
-            [np.vstack([np.sin(2 * np.pi * year_fraction / n),
-                        np.cos(2 * np.pi * year_fraction / n)])
-             for n in range(1, 3)]).T
-        X = np.c_[enso_valid[:, np.newaxis], seasonal_features]
-        return X
+        return np.c_[enso_valid, all_temps]
