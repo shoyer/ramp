a1de074c0017d6c2a04106e9da16510c8d79c919
Stephan Hoyer <shoyer@climate.com>
2015-09-26 14:22:38 -0600

    kernel pca

--- regressor.py
+++ regressor.py
@@ -1,21 +1,26 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, ElasticNet
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA
+
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 
 class Regressor(BaseEstimator):
     def __init__(self):
         self.clf = make_pipeline(
                 StandardScaler(),
-                PCA(n_components=100),
+                KernelPCA(n_components=600, kernel='poly', degree=3),
                 LinearRegression()
+                # ElasticNet(alpha=1e-5, l1_ratio=1),
+                # RandomForestRegressor()
+                # GradientBoostingRegressor(n_estimators=200, max_features="sqrt", max_depth=5)
                 # Earth(max_terms=20, max_degree=10)
         )
 
 
     def fit(self, X, y):
-        self.clf.fit(X, y)
+        self.clf.fit(X, y.ravel())
 
     def predict(self, X):
         return self.clf.predict(X)
